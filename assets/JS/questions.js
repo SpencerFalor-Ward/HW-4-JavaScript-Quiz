700f47225eb96e53f1a95a3d4dee9476ab3a9c62
SpencerFW <srfalorward@ucdavis.edu>
2020-01-22 23:33:28 -0800

    input questions on questions.js

---
+++ assets/JS/questions.js
@@ -0,0 +1,67 @@
+var questions = [
+  {
+    title:
+      "What should appear at the very end of your JavaScript: The < script LANGUAGE = 'JavaScript' > tag ?",
+    choices: [
+      "The </script>",
+      "The <script>",
+      "The END statement",
+      "None of the above"
+    ],
+    answer: "The </script>"
+  },
+
+  {
+    title: "What is the correct JavaScript syntax to write 'Hello World'?",
+    choices: [
+      "System.out.println('Hello World')",
+      "println('Hello World')",
+      "document.write('Hellow World')",
+      "response.write('Hello World')"
+    ],
+    answer: 'document.write("Hello World")'
+  },
+
+  {
+    title: "Which of the following best describes JavaScript ?",
+    choices: [
+      "a low level programming language",
+      "a scripting language precompiled in the browser",
+      "a complicated scripting language",
+      "an object oriented scripting language"
+    ],
+    answer: "an object oriented scripting language"
+  },
+  {
+    title: "Which of the following is the structure of an if statement ?",
+    choices: [
+      "if(conditional expression is true) thenexecute this codeend if",
+      "if(conditional expression is true)execute this codeend if",
+      "if(conditional expression is true)   { then execute this code > ->}",
+      "if(conditional expression is true) then { execute this code }"
+    ],
+    answer:
+      "if(conditional expression is true)   { then execute this code > ->}"
+  },
+  {
+    title: "What is meant by 'this' keyword in javascript ?",
+    choices: [
+      "It refers to current object",
+      "It referes to previous object",
+      "It is a variable which contains value",
+      "None of the above"
+    ],
+    answer: "It refers to current object"
+  },
+
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+];
